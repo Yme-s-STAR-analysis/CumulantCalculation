5afd4582822d718a809d667a54bf82fe78a5bcaf
Yme <yghuang@mails.ccnu.edu.cn>
2024-10-07 17:41:28 +0200

    v7.0: Ratios with CBWC method 1 and 2 are both included in this version Some cosmetic updates

--- Cumulant.cpp
+++ Cumulant.cpp
@@ -13,7 +13,7 @@
 #include "CentManager.h"
 #include "ReweightTool.h"
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
     // Read parameters from command line arguments
     if (argc != 4) {
         std::cout << "[ERROR] Should have 3 arguments!\n";
@@ -64,35 +64,53 @@ int main(int argc, char** argv){
 
     std::cout << "[LOG] Initializing graphes.\n";
     // 22 + 4: ppb, C2/ppb, C3/ppb, C3/C1
-    const int nCums = 26; // cums for cumulant and correlation function (orders) 
+    const int nCums = 39; // cums for cumulant and correlation function (orders) 
     const int nType = 3; // type for proton type
     const char* typeNames[nType] = {"Pro", "Pbar", "Netp"};
     TGraphErrors* tgs[3][nCums];
     TH1D* sCums[3][nCums];
     const char* cumNames[nCums] = {
+        // directly apply CBWC on those quantities
         "C1", "ppb", "C2", "C3", "C4", "C5", "C6", // 7
         "R21", "R2s", "R31", "R32", "R3s", "R42", "R51", "R62", // 15
         "k1", "k2", "k3", "k4", "k5", "k6", // 21
-        "k21", "k31", "k41", "k51", "k61" // 26
+        "k21", "k31", "k41", "k51", "k61", // 26
+        // for those ratios, take ratios of CBWCorrected values
+        "R21r", "R2sr", "R31r", "R32r", "R3sr", "R42r", "R51r", "R62r", // 34
+        "k21r", "k31r", "k41r", "k51r", "k61r" // 39
     };
     const char* cumTitles[nCums] = {
         "C_{1}", "<p+#bar{p}>", "C_{2}", "C_{3}", "C_{4}", "C_{5}", "C_{6}",
         "C_{2}/C_{1}", "C_{2}/<p+#bar{p}>", "C_{3}/C_{1}", "C_{3}/C_{2}", "C_{3}/<p+#bar{p}>", "C_{4}/C_{2}", "C_{5}/C_{1}", "C_{6}/C_{2}",
         "#kappa_{1}", "#kappa_{2}", "#kappa_{3}", "#kappa_{4}", "#kappa_{5}", "#kappa_{6}",
+        "#kappa_{2}/#kappa_{1}", "#kappa_{3}/#kappa_{1}", "#kappa_{4}/#kappa_{1}", "#kappa_{5}/#kappa_{1}", "#kappa_{6}/#kappa_{1}",
+        "C_{2}/C_{1}", "C_{2}/<p+#bar{p}>", "C_{3}/C_{1}", "C_{3}/C_{2}", "C_{3}/<p+#bar{p}>", "C_{4}/C_{2}", "C_{5}/C_{1}", "C_{6}/C_{2}",
         "#kappa_{2}/#kappa_{1}", "#kappa_{3}/#kappa_{1}", "#kappa_{4}/#kappa_{1}", "#kappa_{5}/#kappa_{1}", "#kappa_{6}/#kappa_{1}"
     };
 
-    for (int i=0; i<nType; i++){
-        for (int j=0; j<nCums; j++){
+    for (int i=0; i<nType; i++) {
+        for (int j=0; j<nCums; j++) {
             if (i == 2) { // skip kappa for net proton
-                if (j >= 15) { continue; } 
+                if ((j > = 15 && j <= 25) || j >= 34) { continue; } 
             } else { // skip ppb for proton and antiproton
-                if (j == 1 || j == 8 || j == 11) { continue; }
+                if (j == 1 || j == 8 || j == 11 || j == 27 || j == 30) { continue; }
             }
-            tfin->GetObject(
-                Form("%s%s", typeNames[i], cumNames[j]), 
-                sCums[i][j]
-            );
+            if (j <= 25) { // directly apply CBWC
+                tfin->GetObject(
+                    Form("%s%s", typeNames[i], cumNames[j]), 
+                    sCums[i][j]
+                );
+            } else if (j <= 33) { // cumulant ratios
+                tfin->GetObject(
+                    Form("%s%s", typeNames[i], cumNames[j-19]), 
+                    sCums[i][j]
+                );
+            } else { // kappa ratios
+                tfin->GetObject(
+                    Form("%s%s", typeNames[i], cumNames[j-13]), 
+                    sCums[i][j]
+                );
+            }
             tgs[i][j] = new TGraphErrors(nCent);
             tgs[i][j]->SetName(
                 Form("%s_%s", typeNames[i], cumNames[j])
@@ -109,38 +127,35 @@ int main(int argc, char** argv){
 
     // get number of events
     TH1D* hEntries[nType];
-    for (int i=0; i<nType; i++){
+    for (int i=0; i<nType; i++) {
         tfin->GetObject(Form("%shEntries", typeNames[i]), hEntries[i]);
     }
 
-    for (int i=0; i<nType; i++){ // i for proton types
-        for (int j=0; j<nCums; j++){ // j for cumulant orders
+    for (int i=0; i<nType; i++) { // i for proton types
+        for (int j=0; j<nCums; j++) { // j for cumulant orders
             if (i == 2) { // skip kappa for net proton
-                if (j >= 15) { continue; } 
+                if ((j > = 15 && j <= 25) || j >= 34) { continue; } 
             } else { // skip ppb for proton and antiproton
-                if (j == 1 || j == 8 || j == 11) { continue; }
+                if (j == 1 || j == 8 || j == 11 || j == 27 || j == 30) { continue; }
             }
 
             // loop 1, initialize the arrays to carry values and errors
             double nEvents = 0;
-            for (int k=0; k<nCent; k++){ // k for centralities
+            for (int k=0; k<nCent; k++) { // k for centralities
                 vSum[i][k] = 0;
                 eSum[i][k] = 0;
                 CentEvent[i][k] = 0;
             }
             
             // loop 2, sum up in different centrality bins
-            for (int k=LowMultCut; k<=MaxMult; k++){ // here k for refmult3
+            for (int k=LowMultCut; k<=MaxMult; k++) { // here k for refmult3
                 // Note: 0 is the first bin, +1 is a must
                 nEvents = hEntries[i]->GetBinContent(k+1) * rew8->GetWeight(k+1);
-                if (nEvents < LowEventCut){ continue; }
+                if (nEvents < LowEventCut) { continue; }
 
                 int curCent = cDef->GetCent(k); // get current centrality
-                if (curCent < 0){ continue; } // avoid invalid centrality 
-                if (
-                    (j < 7) || // C1 ~ C6 + ppb
-                    (j >= 15 && j <= 20) // k1 ~ k6
-                ){ // for ratios, the value is just a dividing results, no need to do cbwc
+                if (curCent < 0) { continue; } // avoid invalid centrality 
+                if (j <= 25) { // for those cumulants and ratios, directly apply CBWC
                     vSum[i][curCent] += (sCums[i][j]->GetBinContent(k+1) * nEvents);
                 }
                 // but the errors should be calculated
@@ -149,36 +164,33 @@ int main(int argc, char** argv){
             }
 
             // loop 3, get the averaged results (by dividing number of events)
-            for (int k=0; k<nCent; k++){
+            for (int k=0; k<nCent; k++) {
                 // calculate values
                 if (i == 2) { // skip kappa for net proton
-                    if (j >= 15){ continue; } 
+                    if ((j > = 15 && j <= 25) || j >= 34) { continue; } 
                 } else { // skip ppb for proton and antiproton
-                    if (j == 1 || j == 8 || j == 11) { continue; }
+                    if (j == 1 || j == 8 || j == 11 || j == 27 || j == 30) { continue; }
                 }
-                if (
-                    (j < 7) || 
-                    (j >= 15 && j <= 20)
-                ){
+                if (j <= 25) {
                     vSum[i][k] /= CentEvent[i][k];
-                } else if (j == 7) {
+                } else if (j == 26) {
                     vSum[i][k] = *(tgs[i][2]->GetY()+k) / *(tgs[i][0]->GetY()+k); // C2 / C1
-                } else if (j == 8) {
+                } else if (j == 27) {
                     vSum[i][k] = *(tgs[i][2]->GetY()+k) / *(tgs[i][1]->GetY()+k); // C2 / ppb
-                } else if (j == 9) {
+                } else if (j == 28) {
                     vSum[i][k] = *(tgs[i][3]->GetY()+k) / *(tgs[i][0]->GetY()+k); // C3 / C1
-                } else if (j == 10) {
+                } else if (j == 29) {
                     vSum[i][k] = *(tgs[i][3]->GetY()+k) / *(tgs[i][2]->GetY()+k); // C3 / C2
-                } else if (j == 11) {
+                } else if (j == 30) {
                     vSum[i][k] = *(tgs[i][3]->GetY()+k) / *(tgs[i][1]->GetY()+k); // C3 / ppb
-                } else if (j == 12) {
+                } else if (j == 31) {
                     vSum[i][k] = *(tgs[i][4]->GetY()+k) / *(tgs[i][2]->GetY()+k); // C4 / C2
-                } else if (j == 13) {
+                } else if (j == 32) {
                     vSum[i][k] = *(tgs[i][5]->GetY()+k) / *(tgs[i][0]->GetY()+k); // C5 / C1
-                } else if (j == 14) {
+                } else if (j == 33) {
                     vSum[i][k] = *(tgs[i][6]->GetY()+k) / *(tgs[i][2]->GetY()+k); // C6 / C2
-                } else if (j >= 21) {
-                    vSum[i][k] = *(tgs[i][j-5]->GetY()+k) / *(tgs[i][15]->GetY()+k); // k(2~6) / k1
+                } else if (j >= 34) {
+                    vSum[i][k] = *(tgs[i][j-18]->GetY()+k) / *(tgs[i][15]->GetY()+k); // k(2~6) / k1
                 }
                 // calculate errors
                 eSum[i][k] = sqrt(eSum[i][k]) / CentEvent[i][k];
@@ -192,12 +204,12 @@ int main(int argc, char** argv){
     std::cout << "[LOG] Calculation finished, now saving.\n";
     TFile* tfout = new TFile(Form("cum.cbwc.%s.root", argv[1]), "recreate");
     tfout->cd();
-    for (int i=0; i<nType; i++){
-        for (int j=0; j<nCums; j++){
+    for (int i=0; i<nType; i++) {
+        for (int j=0; j<nCums; j++) {
             if (i == 2) { // skip kappa for net proton
-                if (j >= 15){ continue; } 
+                if ((j > = 15 && j <= 25) || j >= 34) { continue; } 
             } else { // skip ppb for proton and antiproton
-                if (j == 1 || j == 8 || j == 11) { continue; }
+                if (j == 1 || j == 8 || j == 11 || j == 27 || j == 30) { continue; }
             }
             tgs[i][j]->Write();
         }
--- Cumulant4.cpp
+++ Cumulant4.cpp
@@ -13,7 +13,7 @@
 #include "CentManager.h"
 #include "ReweightTool.h"
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
     // Read parameters from command line arguments
     if (argc != 4) {
         std::cout << "[ERROR] Should have 3 arguments!\n";
@@ -65,35 +65,53 @@ int main(int argc, char** argv){
 
     std::cout << "[LOG] Initializing graphes.\n";
     // only 18 cumulants for utmost 4th order
-    const int nCums = 18; // cums for cumulant and correlation function (orders) 
+    const int nCums = 27; // cums for cumulant and correlation function (orders) 
     const int nType = 3; // type for proton type
     const char* typeNames[nType] = {"Pro", "Pbar", "Netp"};
     TGraphErrors* tgs[3][nCums];
     TH1D* sCums[3][nCums];
     const char* cumNames[nCums] = {
+        // directly apply CBWC on those quantities
         "C1", "ppb", "C2", "C3", "C4", // 5
         "R21", "R2s", "R31", "R32", "R3s", "R42", // 11
         "k1", "k2", "k3", "k4", // 15
-        "k21", "k31", "k41" // 18
+        "k21", "k31", "k41", // 18
+        // for those ratios, take ratios of CBWCorrected values
+        "R21r", "R2sr", "R31r", "R32r", "R3sr", "R42r", // 24
+        "k21r", "k31r", "k41r" // 27
     };
     const char* cumTitles[nCums] = {
         "C_{1}", "<p+#bar{p}>", "C_{2}", "C_{3}", "C_{4}",
         "C_{2}/C_{1}", "C_{2}/<p+#bar{p}>", "C_{3}/C_{1}", "C_{3}/C_{2}", "C_{3}/<p+#bar{p}>", "C_{4}/C_{2}",
         "#kappa_{1}", "#kappa_{2}", "#kappa_{3}", "#kappa_{4}", 
+        "#kappa_{2}/#kappa_{1}", "#kappa_{3}/#kappa_{1}", "#kappa_{4}/#kappa_{1}",
+        "C_{2}/C_{1}", "C_{2}/<p+#bar{p}>", "C_{3}/C_{1}", "C_{3}/C_{2}", "C_{3}/<p+#bar{p}>", "C_{4}/C_{2}",
         "#kappa_{2}/#kappa_{1}", "#kappa_{3}/#kappa_{1}", "#kappa_{4}/#kappa_{1}"
     };
 
-    for (int i=0; i<nType; i++){
-        for (int j=0; j<nCums; j++){
+    for (int i=0; i<nType; i++) {
+        for (int j=0; j<nCums; j++) {
             if (i == 2) { // skip kappa for net proton
-                if (j >= 11) { continue; } 
+                if ((j >= 11 && j <= 17) || j >= 24) { continue; } 
             } else { // skip ppb for proton and antiproton
-                if (j == 1 || j == 6 || j == 9) { continue; }
+                if (j == 1 || j == 6 || j == 9 || j == 19 || j == 22) { continue; }
             }
-            tfin->GetObject(
-                Form("%s%s", typeNames[i], cumNames[j]), 
-                sCums[i][j]
-            );
+            if (j <= 17) { // directly apply CBWC
+                tfin->GetObject(
+                    Form("%s%s", typeNames[i], cumNames[j]), 
+                    sCums[i][j]
+                );
+            } else if (j <= 23) { // cumulant ratios
+                tfin->GetObject(
+                    Form("%s%s", typeNames[i], cumNames[j-13]), 
+                    sCums[i][j]
+                );
+            } else { // kappa ratios
+                tfin->GetObject(
+                    Form("%s%s", typeNames[i], cumNames[j-9]), 
+                    sCums[i][j]
+                );
+            }
             tgs[i][j] = new TGraphErrors(nCent);
             tgs[i][j]->SetName(
                 Form("%s_%s", typeNames[i], cumNames[j])
@@ -110,38 +128,35 @@ int main(int argc, char** argv){
 
     // get number of events
     TH1D* hEntries[nType];
-    for (int i=0; i<nType; i++){
+    for (int i=0; i<nType; i++) {
         tfin->GetObject(Form("%shEntries", typeNames[i]), hEntries[i]);
     }
 
-    for (int i=0; i<nType; i++){ // i for proton types
-        for (int j=0; j<nCums; j++){ // j for cumulant orders
+    for (int i=0; i<nType; i++) { // i for proton types
+        for (int j=0; j<nCums; j++) { // j for cumulant orders
             if (i == 2) { // skip kappa for net proton
-                if (j >= 11) { continue; } 
+                if ((j >= 11 && j <= 17) || j >= 24) { continue; } 
             } else { // skip ppb for proton and antiproton
-                if (j == 1 || j == 6 || j == 9) { continue; }
+                if (j == 1 || j == 6 || j == 9 || j == 19 || j == 22) { continue; }
             }
 
             // loop 1, initialize the arrays to carry values and errors
             double nEvents = 0;
-            for (int k=0; k<nCent; k++){ // k for centralities
+            for (int k=0; k<nCent; k++) { // k for centralities
                 vSum[i][k] = 0;
                 eSum[i][k] = 0;
                 CentEvent[i][k] = 0;
             }
             
             // loop 2, sum up in different centrality bins
-            for (int k=LowMultCut; k<=MaxMult; k++){ // here k for refmult3
+            for (int k=LowMultCut; k<=MaxMult; k++) { // here k for refmult3
                 // Note: 0 is the first bin, +1 is a must
                 nEvents = hEntries[i]->GetBinContent(k+1) * rew8->GetWeight(k+1);
-                if (nEvents < LowEventCut){ continue; }
+                if (nEvents < LowEventCut) { continue; }
 
                 int curCent = cDef->GetCent(k); // get current centrality
-                if (curCent < 0){ continue; } // avoid invalid centrality 
-                if (
-                    (j < 5) || // C1 ~ C4 + ppb
-                    (j >= 11 && j <= 14) // k1 ~ k4
-                ){ // for ratios, the value is just a dividing results, no need to do cbwc
+                if (curCent < 0) { continue; } // avoid invalid centrality 
+                if (j <= 17) { // for those cumulants and ratios, directly apply CBWC
                     vSum[i][curCent] += (sCums[i][j]->GetBinContent(k+1) * nEvents);
                 }
                 // but the errors should be calculated
@@ -150,32 +165,29 @@ int main(int argc, char** argv){
             }
 
             // loop 3, get the averaged results (by dividing number of events)
-            for (int k=0; k<nCent; k++){
+            for (int k=0; k<nCent; k++) {
                 // calculate values
                 if (i == 2) { // skip kappa for net proton
-                    if (j >= 11){ continue; } 
+                    if ((j >= 11 && j <=17) || j >= 24) { continue; } 
                 } else { // skip ppb for proton and antiproton
-                    if (j == 1 || j == 6 || j == 9) { continue; }
+                    if (j == 1 || j == 6 || j == 9 || j == 19 || j == 22) { continue; }
                 }
-                if (
-                    (j < 5) || 
-                    (j >= 11 && j <= 14)
-                ){
+                if (j <= 17) {
                     vSum[i][k] /= CentEvent[i][k];
-                } else if (j == 5) {
+                } else if (j == 18) {
                     vSum[i][k] = *(tgs[i][2]->GetY()+k) / *(tgs[i][0]->GetY()+k); // C2 / C1
-                } else if (j == 6) {
+                } else if (j == 19) {
                     vSum[i][k] = *(tgs[i][2]->GetY()+k) / *(tgs[i][1]->GetY()+k); // C2 / ppb
-                } else if (j == 7) {
+                } else if (j == 20) {
                     vSum[i][k] = *(tgs[i][3]->GetY()+k) / *(tgs[i][0]->GetY()+k); // C3 / C1
-                } else if (j == 8) {
+                } else if (j == 21) {
                     vSum[i][k] = *(tgs[i][3]->GetY()+k) / *(tgs[i][2]->GetY()+k); // C3 / C2
-                } else if (j == 9) {
+                } else if (j == 22) {
                     vSum[i][k] = *(tgs[i][3]->GetY()+k) / *(tgs[i][1]->GetY()+k); // C3 / ppb
-                } else if (j == 10) {
+                } else if (j == 23) {
                     vSum[i][k] = *(tgs[i][4]->GetY()+k) / *(tgs[i][2]->GetY()+k); // C4 / C2
-                } else if (j >= 15) {
-                    vSum[i][k] = *(tgs[i][j-3]->GetY()+k) / *(tgs[i][11]->GetY()+k); // k(2~6) / k1
+                } else if (j >= 24) {
+                    vSum[i][k] = *(tgs[i][j-12]->GetY()+k) / *(tgs[i][11]->GetY()+k); // k(2~4) / k1
                 }
                 // calculate errors
                 eSum[i][k] = sqrt(eSum[i][k]) / CentEvent[i][k];
@@ -189,12 +201,12 @@ int main(int argc, char** argv){
     std::cout << "[LOG] Calculation finished, now saving.\n";
     TFile* tfout = new TFile(Form("cum.cbwc.%s.root", argv[1]), "recreate");
     tfout->cd();
-    for (int i=0; i<nType; i++){
-        for (int j=0; j<nCums; j++){
+    for (int i=0; i<nType; i++) {
+        for (int j=0; j<nCums; j++) {
             if (i == 2) { // skip kappa for net proton
-                if (j >= 11){ continue; } 
+                if ((j >= 11 && j <=17) || j >= 24) { continue; } 
             } else { // skip ppb for proton and antiproton
-                if (j == 1 || j == 6 || j == 9) { continue; }
+                if (j == 1 || j == 6 || j == 9 || j == 19 || j == 22) { continue; }
             }
             tgs[i][j]->Write();
         }
